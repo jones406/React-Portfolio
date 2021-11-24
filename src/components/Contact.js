96be442316fa2ae73a20d4206689fa10412c3865
brooke <brookejones406@gmail.com>
2021-11-24 00:11:57 -0600

    form functioning

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,53 @@
-import React from 'react';
+
 import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import './style.css';
 
 function Contact() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+    return name === 'firstName' ? setFirstName(value) : setLastName(value);
+    };
+
+    const handleFormSubmit = (e) => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        e.preventDefault();
+
+        // Alert the user their first and last name, clear the inputs
+        alert(`Hello ${firstName} ${lastName} your form has been successfully sent!`);
+        setFirstName('');
+        setLastName('');
+    };
 
     return (
         <div>
-            <h1>HELLOWWWW</h1>
-            <p>I'ma put a form here</p>
-            <Button color="secondary">Send</Button>
-        </div>
+            <p>
+                Hello {firstName} {lastName}
+            </p>
+            <form className="form">
+                <input
+                    value={firstName}
+                    name="firstName"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="First Name"
+                />
 
+                <input
+                    value={lastName}
+                    name="lastName"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Last Name"
+                />
+
+                <Button type="button" onClick={handleFormSubmit} color="secondary">SUBMIT</Button>
+            </form>
+        </div>
     );
 }
 
